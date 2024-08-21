187b32d3f423cff0096550fda053fd1edf140d7a
wisesam <lalasjhan@gmail.com>
2024-08-21 18:07:40 +0900

    Counseling list. To be improved later

---
+++ src/lib/components/status-chip-counseling.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { CounselingStatus } from '../types/index.d.js';
+
+	export let status: CounselingStatus = CounselingStatus.Planned;
+	const color = status === CounselingStatus.Planned ? 'magenta' : 
+				(status === CounselingStatus.Canceled ? 'gray' : 'green');
+</script>
+
+<div style="background-color: {color}" class="chip">{status}</div>
+
+<style>
+	.chip {
+		width: 85px;
+		height: 24px;
+		flex-grow: 0;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+        color: white;
+		padding: 0px 4px;
+		border-radius: 100px;
+        font-size: smaller;
+	}
+</style>
