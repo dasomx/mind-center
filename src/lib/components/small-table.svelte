47e62066a8f72fe63f32ecd2b4e66927128e8d4a
choipd <mj@dasomx.com>
2024-08-25 16:37:25 +0900

    dashboard에서 client 눌러서 바로 수정 기능 Fixes #50

--- src/lib/components/small-table.svelte
+++ src/lib/components/small-table.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { convertTimestampToDateString } from '$lib/firebase/utils';
-import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+	import Button from '@smui/button';
+	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
 	import { Timestamp } from 'firebase/firestore';
-	export let data: { name: string; createdAt: Timestamp }[] = [
-		{ name: 'Ayesha', createdAt: Timestamp.fromDate(new Date('15-03-2024')) },
-		{ name: 'Ali', createdAt: Timestamp.fromDate(new Date('22-07-2023')) },
-		{ name: 'Zara', createdAt: Timestamp.fromDate(new Date('05-11-2024')) },
-		{ name: 'Ahmed', createdAt: Timestamp.fromDate(new Date('30-08-2023')) },
-		{ name: 'Fatima', createdAt: Timestamp.fromDate(new Date('18-12-2024')) },
-		{ name: 'Hassan', createdAt: Timestamp.fromDate(new Date('09-05-2024')) }
+	export let data: { id: string; name: string; createdAt: Timestamp }[] = [
+		{ id: '123', name: 'Ayesha', createdAt: Timestamp.fromDate(new Date('15-03-2024')) },
+		{ id: '234', name: 'Ali', createdAt: Timestamp.fromDate(new Date('22-07-2023')) },
+		{ id: '345', name: 'Zara', createdAt: Timestamp.fromDate(new Date('05-11-2024')) },
+		{ id: '456', name: 'Ahmed', createdAt: Timestamp.fromDate(new Date('30-08-2023')) },
+		{ id: '567', name: 'Fatima', createdAt: Timestamp.fromDate(new Date('18-12-2024')) },
+		{ id: '678', name: 'Hassan', createdAt: Timestamp.fromDate(new Date('09-05-2024')) }
 	];
+	export let resource: 'clients' | 'counselings' | 'links';
 </script>
 
 <DataTable table$aria-label="People list" style="width: 100%; border: 0px">
@@ -20,9 +23,18 @@ import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
 		</Row>
 	</Head>
 	<Body>
-		{#each data as { name, createdAt }}
-			<Row class="content-row">
-				<Cell>{name}</Cell>
+		{#each data as { id, name, createdAt }}
+			<Row
+				class="content-row"
+				style="cursor: pointer"
+				on:click={() => {
+					console.log('id', id);
+					goto(`/mc/${resource}/${id}`);
+				}}
+			>
+				<Cell>
+					{name}
+				</Cell>
 				<Cell>{convertTimestampToDateString(createdAt)}</Cell>
 			</Row>
 		{/each}
